956697ed4e3d99459c50e8c0c9a3f2f45f5f6326
madanadra <mlaksindra@gmail.com>
2024-12-23 20:42:19 +0700

    final

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/travel' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/travel/' : '/',
   plugins: [react()],
 })
